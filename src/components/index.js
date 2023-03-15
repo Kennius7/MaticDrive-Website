f34ad05e998f9343331495790506c779acf87098
Kenniusboggs <ogbogukenny@yahoo.com>
2023-03-15 10:14:28 +0100

    Import cleanups

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,7 @@
 import Navbar from "./Navbar";
 import Features from "./Features";
-import CardDeal from "./CardDeal";
 import AboutUs from "./AboutUs";
-import Clients from "./Clients";
 import Partnership from "./Partnership";
-import Stats from "./Stats";
 import Footer from "./Footer";
 import TeamPlayers from "./TeamPlayers";
 import Hero from "./Hero";
@@ -15,11 +12,8 @@ import ContactUs from "./ContactUs";
 export {
   Navbar,
   Features,
-  CardDeal,
   AboutUs,
-  Clients,
   Partnership,
-  Stats,
   Footer,
   TeamPlayers,
   Hero,
